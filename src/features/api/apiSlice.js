6174e2c269fbf8828418aaa69dd034fbf38f1709
saju575 <sakilahmedsaju575@gmail.com>
2024-01-28 14:30:32 +0600

    api url change

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: "https://task-manager-b3k8.onrender.com/api/v1",
   }),
   endpoints: (builder) => ({}),
 });
